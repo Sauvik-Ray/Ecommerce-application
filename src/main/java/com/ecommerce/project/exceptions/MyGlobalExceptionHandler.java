825e3169912eafa52614b9e6d181a311ba78fbf3
Sauvik <rsauvik9@gmail.com>
2025-09-10 08:55:57 +0530

    Updated globalexceptionhandler

--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ecommerce.project.exceptions;
 
 import org.springframework.http.HttpStatus;
@@ -7,22 +6,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.*;
-
-=======
-package com.ecommerce.project.exceptions;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
 import java.util.*;
 
 @RestControllerAdvice
@@ -44,10 +29,10 @@ public class MyGlobalExceptionHandler {
         return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(APIException.class)
-    public ResponseEntity<String>APIException(ResourceNotFoundException e){
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<String>APIException(ApiException e){
         String message = e.getMessage();
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
->>>>>>> 3af3c01487177931ae4e23f75c4ec97c2d2c8d7d
+
